50a9908dfa27cd55e960e1902b650119b6cf1c18
MaxT-gh <m7720613@gmail.com>
2021-08-02 14:42:43 +0300

    task solutions

---
+++ src/PalindromeSolution.java
@@ -0,0 +1,28 @@
+public class PalindromeSolution {
+    // Find the palindrome
+
+    public static void main(String[] args) {
+        String s = "123255359";
+        int maxLength = 1;
+        int startPosition = 0;
+        int endPosition = 0;
+        for (int n = 1; n <= s.length(); n++) {
+            for (int i = 0; i <= s.length() - n; i++) {
+                String subs = s.substring(i, i + n);
+                if (subs.equals(new StringBuilder(subs).reverse().toString())) {
+                    maxLength = n;
+                    startPosition = i;
+                    endPosition = i + n;
+                    break;
+                }
+            }
+        }
+        if (maxLength == 1) {
+            System.out.println("Palindrome not found");
+        } else {
+            System.out.println("Palindrome length = " + maxLength);
+            System.out.println(s.substring(startPosition, endPosition));
+        }
+    }
+}
+
